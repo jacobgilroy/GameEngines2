c16ca7cc42d2fd1fb85d3a6a58f1519d955a8e11
jacobgilroy <c13505347@mydit.ie>
2017-05-05 17:11:00 +0100

    Add files via upload

---
+++ Assets/Atom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atom : MonoBehaviour {
+
+    public string electronsPerShell;
+    public string atom;
+    public GameObject protonPrefab;
+    public GameObject electronPrefab;
+
+    // Use this for initialization
+    void Start () {
+
+        electronsPerShell = "1";
+        atom = "Hydrogen";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Boid.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+    List<SteeringBehaviour> behaviours = List<SteeringBehaviour>();
+
+    public Vector3 force = Vector3.zero;
+    public Vector3 acceleration = Vector3.zero;
+    public VEctor3 velocity = Vector3.zero;
+    public int mass = 1;
+    public float maxSpeed = 10.0f;
+    public float distFromNucleus;
+
+	// Use this for initialization
+	void Start () {
+
+        SteeringBehaviour[] behaviours = GetComponent<SteeringBehaviour>();
+
+        foreach (SteeringBehaviour b in behaviours)
+        {
+            this.behaviours.Add(b);
+        }
+	}
+
+    public Vector3 seekForce(Vector3 target)
+    {
+        Vector3 between = target - transform.position;
+        between.normalize();
+        between *= maxSpeed;
+        return between - velocity;
+    }
+
+    public Vector3 Calculate()
+    {
+        force = Vector3.zero;
+
+        foreach (SteeringBehaviour b in behaviours)
+        {
+            if (b.isActiveAndEnabled)
+            {
+                force += b.calculate() * b.weight;
+            }
+        }
+
+        return force;
+    }
+    
+	
+	// Update is called once per frame
+	void Update () {
+
+        force = Calculate();
+
+
+	}
+}
